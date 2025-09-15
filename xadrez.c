a6dc0aa003ba90b5f58b23ffedd1eefce9ee5c58
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2025-09-15 20:14:55 -0300

    xadrez nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    int movimentacaoTorre, movimentacaoBispo, movimentacaoRainha;
+    int movimentacaoTorre, movimentacaoBispo, movimentacaoRainha, movimentacaoCavalo;
 
     printf("Insira o número de movimentos da torre:\n");
     scanf("%d", &movimentacaoTorre);
-    // Torre move 5 para a direita
+    // Movimentaçao da torre
     for (int i = 0; i < movimentacaoTorre; i++)
     {
         printf("Torre moveu para a direita\n", i);
@@ -14,7 +14,7 @@ int main(){
     printf("Insira o número de movimentos do bispo:\n");
     scanf("%d", &movimentacaoBispo);
     int j = 0;
-    //Bispo move 5 na diagonal cima, direita
+    //Movimentação do bispo
     while(j < movimentacaoBispo){
         printf("Bispo move para cima, direita\n");
         j++;
@@ -23,9 +23,26 @@ int main(){
     printf("Insira o número de movimentos da rainha:\n");
     scanf("%d", &movimentacaoRainha);
     int k = 0;
-    //Rainha move 8 para esquerda
+    //Movimentação da rainha
     do{
         printf("Rainha se move para esquerda\n");
         k++;
     } while (k < movimentacaoRainha);
+
+    printf("Insira a quantidade de movimentos do cavalo:\n");
+    scanf("%d", &movimentacaoCavalo);
+    //Movimentação do cavalo
+    for (int i = 0; i < movimentacaoCavalo; i++){
+        int j = 0;
+        do{
+            printf("Cavalo move para cima\n");
+            j++;
+        } while (j < 2);
+        int k = 0;
+        while (k < 1)
+        {
+            printf("Cavalo move para a direita\n");
+            k++;
+        };
+    }
 }

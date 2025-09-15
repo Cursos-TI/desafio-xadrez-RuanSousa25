91de6047b79d67964cc479eb57db97fc66438ac3
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2025-09-15 20:33:18 -0300

    xadrez nível mestre

--- xadrez.c
+++ xadrez.c
@@ -3,46 +3,69 @@
 int main(){
     int movimentacaoTorre, movimentacaoBispo, movimentacaoRainha, movimentacaoCavalo;
 
+    // Movimentaçao da torre
+    void movimentaTorre(int numeroMovimento){
+        printf("Torre moveu para a direita\n");
+        if(numeroMovimento-1 > 0){
+            movimentaTorre(numeroMovimento - 1);
+        };
+    };
+    //Movimentação do bispo
+    void movimentaBispo(int numeroMovimento){
+        for (int i = 0; i < 1; i++){
+            printf("Bispo move para a cima\n");
+            for (int j = 0; j < 1; j++){
+                printf("Bispo move para a direita\n");
+            }
+        }
+        if(numeroMovimento-1 > 0){
+            movimentaBispo(numeroMovimento - 1);
+        }
+    }
+    //movimentação da rainha
+    void movimentaRainha(int numeroMovimento){
+        printf("Rainha moveu para a cima\n");
+        if(numeroMovimento-1 > 0){
+            movimentaRainha(numeroMovimento - 1);
+        };
+    };
+
     printf("Insira o número de movimentos da torre:\n");
     scanf("%d", &movimentacaoTorre);
-    // Movimentaçao da torre
-    for (int i = 0; i < movimentacaoTorre; i++)
-    {
-        printf("Torre moveu para a direita\n", i);
-    };
+    
+    movimentaTorre(movimentacaoTorre);
 
     printf("Insira o número de movimentos do bispo:\n");
     scanf("%d", &movimentacaoBispo);
-    int j = 0;
-    //Movimentação do bispo
-    while(j < movimentacaoBispo){
-        printf("Bispo move para cima, direita\n");
-        j++;
-    };
+    movimentaBispo(movimentacaoBispo);
 
     printf("Insira o número de movimentos da rainha:\n");
     scanf("%d", &movimentacaoRainha);
-    int k = 0;
-    //Movimentação da rainha
-    do{
-        printf("Rainha se move para esquerda\n");
-        k++;
-    } while (k < movimentacaoRainha);
+    movimentaRainha(movimentacaoRainha);
 
     printf("Insira a quantidade de movimentos do cavalo:\n");
     scanf("%d", &movimentacaoCavalo);
+
     //Movimentação do cavalo
-    for (int i = 0; i < movimentacaoCavalo; i++){
-        int j = 0;
+    for (int m = 0; m < movimentacaoCavalo; m++){
+        int vertical = 0;
         do{
             printf("Cavalo move para cima\n");
-            j++;
-        } while (j < 2);
-        int k = 0;
-        while (k < 1)
-        {
-            printf("Cavalo move para a direita\n");
-            k++;
-        };
+            vertical++;
+
+            int horizontal = 0;
+            while (horizontal < 1)
+            {
+                if(vertical < 2){
+                    break;
+                }
+                printf("Cavalo move para a direita\n");
+                horizontal++;
+            };
+
+            if(vertical<2){
+                continue;
+            }
+        } while (vertical < 2);
     }
 }
